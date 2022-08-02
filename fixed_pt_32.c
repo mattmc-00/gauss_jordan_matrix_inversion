43a593ccec3859750c3d6a6fed58fffce3beab4c
MatthewMcLean00 <50306715+MatthewMcLean00@users.noreply.github.com>
2022-08-02 10:51:24 -0700

    Found and fixed problem with column indexing

--- fixed_pt_32.c
+++ fixed_pt_32.c
@@ -17,42 +17,6 @@ typedef int32_t FX_15_16;
 #define FX_DIV(a,b) (int32_t)(((int64_t)a << 16) / ((int64_t)b))
 #define FX_REM(a,b) ((a%b))
 
-void print_a(FX_15_16* a[], int n){
-  FX_15_16* ptr;
-  int i, j;
-  printf("Input Matrix:\nA = [");
-  for (i = 0; i < n; i++) {
-    ptr = a[i];
-    printf("\t");
-    for (j = 0; j < n; j++) {
-      printf("%f, ", FX_TO_FL(*(ptr + j)));
-    }
-    if(i == (n - 1)){
-      printf("]\n");
-    } else {
-      printf("\n");
-    }
-  }
-}
-
-void print_inv(FX_15_16* a[], int n, int indices[]){
-  FX_15_16* ptr;
-  int i, j;
-  printf("Input Matrix:\nA = [");
-  for (i = 0; i < n; i++) {
-    ptr = a[i];
-    printf("\t");
-    for (j = 0; j < n; j++) {
-      printf("%f, ", FX_TO_FL(*(ptr + indices[j])));
-    }
-    if(i == (n - 1)){
-      printf("]\n");
-    } else {
-      printf("\n");
-    }
-  }
-}
-
 int main(){
   int n;
   printf("Input order of matrix:\n");
@@ -60,6 +24,7 @@ int main(){
 
   FX_15_16* a[n];
   int i, j;
+
   for (i = 0; i < n; i++) {
     a[i] = malloc(sizeof(FX_15_16) * n);
   }
@@ -68,75 +33,106 @@ int main(){
   FX_15_16 input_int;
   float input;
   int indices[n];
+
   for (i = 0; i < n; i++) {
+    printf("Row %d:\n", i);
     ptr = a[i];
     indices[i] = i;
     for (j = 0; j < n; j++) {
-      printf("Input a[%d][%d] as float, then enter:\n", i, j);
       scanf("%f", &input);
       input_int = FL_TO_FX(input);
       *(ptr + j) = input_int;
     }
   }
-  printf("input\n");
-  print_a(a, n);
+  printf("\n*******************************\n\n");
+
+  printf("A = [\n");
+  for (i = 0; i < n; i++) {
+    ptr = a[i];
+    for (j = 0; j < n; j++) {
+      input = FX_TO_FL(*(ptr + j));
+      printf("\t%f", input);
+    }
+    printf("\n");
+  }
+  printf("]\n");
+  printf("\n*******************************\n\n");
 
   FX_15_16 pivot_vector[n];
   FX_15_16* pivot_ptr;
   FX_15_16* swap_row;
   FX_15_16 one = INT_TO_FX(1);
   FX_15_16 curr, max, sub, pivot_inv;
-  int ii, jj, max_i;
+  int norm, reduce, max_i;
 
-  for(i = 0; i < n; i++) {
+  for (norm = 0; norm < n; norm++) {
     //first perform swaps to bring max of column to pivot row
-    max_i = i;
-    max = abs(*(a[i] + i));
-    for(ii = i + 1; ii < n; ii++){
-      curr = abs(*(a[ii] + i));
+    max_i = norm;
+    max = abs(*(a[norm] + norm));
+    for(i = norm + 1; i < n; i++){
+      curr = abs(*(a[i] + norm));
       if(curr > max){
-        max_i = ii;
+        max_i = i;
         max = curr;
       }
     }
 
-    if(i != max_i){
-      swap_row = a[i];
-      ii = indices[i];
-      a[i] = a[max_i];
-      indices[i] = indices[max_i];
+    if(norm != max_i){
+      swap_row = a[norm];
+      i = indices[norm];
+      a[norm] = a[max_i];
+      indices[norm] = indices[max_i];
       a[max_i] = swap_row;
-      indices[max_i] = ii;
+      indices[max_i] = i;
     }
 
-    //pivot_vector[i] == pivot for this iteration
-    pivot_ptr = a[i];
-    pivot_vector[i] = *(pivot_ptr + i);
-    *(pivot_ptr + i) = one;
+    //pivot_vector[norm] == pivot for this iteration
+    pivot_ptr = a[norm];
+    pivot_vector[norm] = *(pivot_ptr + norm);
+    *(pivot_ptr + norm) = one;
 
     //normalize pivot row
-    pivot_inv = FX_DIV(one, pivot_vector[i]);
-    for (jj = 0; jj < n; jj++) {
-      *(pivot_ptr + jj) = FX_MUL(*(pivot_ptr + jj), pivot_inv);
+    pivot_inv = FX_DIV(one, pivot_vector[norm]);
+    for (j = 0; j < n; j++) {
+      *(pivot_ptr + j) = FX_MUL(*(pivot_ptr + j), pivot_inv);
     }
 
-    //reduce non-pivot rows
-    for (j = 0; j < n; j++) {
-      if (j != i) {
-        ptr = a[j];
-        //pivot_vector[j] == current row's pivot
-        pivot_vector[j] = *(ptr + i);
-        *(ptr + i) = 0;
+    for (reduce = 0; reduce < n; reduce++) {
+      if (reduce != norm) {
+        ptr = a[reduce];
+        pivot_vector[reduce] = *(ptr + norm);
+        //pivot_vector[reduce] == current row's pivot
+        *(ptr + norm) = 0;
 
-        for (jj = 0; jj < n; jj++) {
-          sub = FX_MUL(*(pivot_ptr + jj), pivot_vector[j]);
-          *(ptr + jj) = FX_SUB(*(ptr + jj), sub);
+        //reduce non-pivot rows
+        for (i = 0; i < n; i++) {
+          sub = FX_MUL(*(pivot_ptr + i), pivot_vector[reduce]);
+          *(ptr + i) = FX_SUB(*(ptr + i), sub);
         }
       }
     }
   }
-  printf("done\n");
-  print_inv(a, n, indices);
+
+  printf("\n*******************************\n\n");
+
+  int print_cols[n];
+  for (i = 0; i < n; i++) {
+    print_cols[indices[i]] = i;
+  }
+
+  int indexed_col;
+  printf("A-inv = [\n");
+  for (i = 0; i < n; i++) {
+    ptr = a[i];
+    for (j = 0; j < n; j++) {
+      indexed_col = print_cols[j];
+      input = FX_TO_FL(*(ptr + indexed_col));
+      printf("\t%f", input);
+    }
+    printf("\n");
+  }
+  printf("]\n");
+  printf("\n*******************************\n\n");
 
   return(0);
 }
